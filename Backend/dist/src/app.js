39a52304e8bd72028184296b37a09d28e7ed9e81
Sounarva Bardhan <sounarvabardhan2003@gmail.com>
2026-04-08 14:15:37 +0530

    Minor Bug Fixed 06

--- Backend/dist/src/app.js
+++ Backend/dist/src/app.js
@@ -29,11 +29,11 @@ app.use(cookieParser());
 //     methods: ["GET", "POST"],
 //     credentials: true
 // }))
-const publicPath = path.join(__dirname, "../public");
+const publicPath = path.resolve(process.cwd(), "public");
 app.use(express.static(publicPath));
 app.use("/api/v1/ai", aiRoutes);
 app.use("/api/v1/auth", userRoutes);
-app.get("*name", (req, res, next) => {
+app.get("*", (req, res, next) => {
     if (req.originalUrl.startsWith("/api")) {
         return next();
     }
